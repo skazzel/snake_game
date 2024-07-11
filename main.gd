1b5742d0b76f1d6690516453ed5bca3005edc799
skazzel <skazzel@seznam.cz>
2024-07-11 23:11:04 +0200

    main fix

--- main.gd
+++ main.gd
@@ -13,6 +13,7 @@ var oldData : Array
 var snakeData : Array
 var snake : Array
 
+# food positions
 var foodPos : Vector2
 var regenFood : bool = true
 
@@ -29,11 +30,11 @@ func _ready():
 	new_game()
 	
 func new_game():
-	get_tree().call_group("segments", "queue_free")
+	get_tree().call_group( "segments", "queue_free" )
 	get_tree().paused = false
 	%Timer.start()
 	%gameOver.hide()
-	%score.get_node("scoreLabel").text = "Score: " + str(score)
+	%score.get_node( "scoreLabel" ).text = "Score: " + str( score )
 	moveDirection = up
 	canMove = true
 	generate_snake()
@@ -44,37 +45,37 @@ func generate_snake():
 	snakeData.clear()
 	snake.clear()
 	
-	for i in range(3):
-		add_segment(startPos + Vector2(0, i))
+	for i in range( 3 ):
+		add_segment( startPos + Vector2( 0, i ) )
 		
-func add_segment(pos):
-	snakeData.append(pos)
+func add_segment( pos ):
+	snakeData.append( pos )
 	var snakeSegment = snakeScene.instantiate()
-	snakeSegment.position = (pos * cell_size) + Vector2(0, cell_size)
-	add_child(snakeSegment)
-	snake.append(snakeSegment)
+	snakeSegment.position = ( pos * cell_size ) + Vector2( 0, cell_size )
+	add_child( snakeSegment )
+	snake.append( snakeSegment )
 	
-func _process(delta):
+func _process( delta ):
 	move_snake()
 	
 func move_snake():
 	if canMove:
-		if Input.is_action_just_pressed("down") and moveDirection != up:
+		if Input.is_action_just_pressed( "down" ) and moveDirection != up:
 			moveDirection = down
 			canMove = false
 			if not game_started:
 				game_started()
-		if Input.is_action_just_pressed("up") and moveDirection != down:
+		if Input.is_action_just_pressed( "up" ) and moveDirection != down:
 			moveDirection = up
 			canMove = false
 			if not game_started:
 				game_started()
-		if Input.is_action_just_pressed("left") and moveDirection != right:
+		if Input.is_action_just_pressed( "left" ) and moveDirection != right:
 			moveDirection = left
 			canMove = false
 			if not game_started:
 				game_started()
-		if Input.is_action_just_pressed("right") and moveDirection != left:
+		if Input.is_action_just_pressed( "right" ) and moveDirection != left:
 			moveDirection = right
 			canMove = false
 			if not game_started:
@@ -88,10 +89,10 @@ func _on_timer_timeout():
 	canMove = true
 	oldData = [] + snakeData
 	snakeData[0] += moveDirection
-	for i in range(len(snakeData)):
+	for i in range( len( snakeData ) ):
 		if i > 0:
 			snakeData[i] = oldData[i - 1]
-		snake[i].position = (snakeData[i] * cell_size) + Vector2(0, cell_size)
+		snake[i].position = ( snakeData[i] * cell_size ) + Vector2( 0, cell_size )
 	check_out_of_bounds()
 	check_self_eaten()
 	check_food_eaten()
@@ -101,7 +102,7 @@ func check_out_of_bounds():
 		end_game()
 
 func check_self_eaten():
-	for i in range(1, len(snakeData)):
+	for i in range( 1, len( snakeData ) ):
 		if snakeData[0] == snakeData[i]:
 			end_game()
 		
@@ -109,18 +110,18 @@ func move_food():
 	regenFood = true
 	while regenFood:
 		regenFood = false
-		foodPos = Vector2(randi_range(0, cells - 1), randi_range(0, cells - 1))
+		foodPos = Vector2( randi_range( 0, cells - 1 ), randi_range( 0, cells - 1 ) )
 		for i in snakeData:
 			if foodPos == i:
 				regenFood = true
-	$food.position = (foodPos * cell_size) + Vector2(0, cell_size)
+	$food.position = ( foodPos * cell_size ) + Vector2( 0, cell_size )
 	regenFood = true
 
 func check_food_eaten():
 	if snakeData[0] == foodPos:
 		score += 1
-		%score.get_node("scoreLabel").text = "Score: " + str(score)
-		add_segment(oldData[-1])
+		%score.get_node( "scoreLabel" ).text = "Score: " + str( score )
+		add_segment( oldData[-1] )
 		move_food()
 
 func end_game():
